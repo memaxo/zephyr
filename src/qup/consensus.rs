df381f9197c929c9e44572cc6a642a4061229927
jackmazac <mazac@chapman.edu>
2024-06-11 18:34:43 -0700

    Increased the validator threshold for the secure consensus mechanism.

--- src/qup/consensus.rs
+++ src/qup/consensus.rs
@@ -147,8 +147,6 @@ fn adapt_consensus_algorithm(&mut self) {
                 self.consensus_mechanism = ConsensusMechanism::Secure;
                 self.config.validator_threshold = 0.75; // Increase validator threshold for higher security
             }
-            classical_node,
-            quantum_node,
         }
     }
 
